339b68663ab6c3bf646bb7cbe5bcc33fadaac3d0
mcwelser <s2310307092@fhooe.at>
2025-11-27 16:23:21 +0100

    add e2e tests

---
+++ Deo.WebApp.Tests/Deo.WebApp.Tests/CounterPageTests.cs
@@ -0,0 +1,67 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace Deo.WebApp.Tests;
+
+public class CounterPageTests : PageTest
+{
+    const string DEO_FRONTEND_URL_ENV_VAR_NAME = "DEO_FRONTEND_URL";
+
+    private string? frontendUrl;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        PlaywrightHelpers.InstallDeps();
+        PlaywrightHelpers.Install();
+
+        frontendUrl = Environment.GetEnvironmentVariable("DEO_FRONTEND_URL")
+            ?? throw new InvalidOperationException($"Environment variable \"{DEO_FRONTEND_URL_ENV_VAR_NAME}\" not set.");
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        // not needed for now
+    }
+
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new()
+        {
+            BaseURL = frontendUrl
+        };
+    }
+
+    [Test]
+    public async Task CounterStartsAtZero()
+    {
+        await Page.GotoAsync("/", new() { WaitUntil = WaitUntilState.NetworkIdle });
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Service Counter" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Reset" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Status)).ToHaveTextAsync("Current count: 0");
+    }
+
+    [Test]
+    public async Task CounterIncrements()
+    {
+        await Page.GotoAsync("/", new() { WaitUntil = WaitUntilState.NetworkIdle });
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Service Counter" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Reset" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Status)).ToHaveTextAsync("Current count: 0");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Increment" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Status)).ToHaveTextAsync("Current count: 1");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // not needed for now
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        // not needed for now
+    }
+}
